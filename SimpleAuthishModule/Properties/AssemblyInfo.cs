f64957a3901c7ec93b822a4c008ba90ce2c7b461
Mads Nyborg <nyborg.mads@gmail.com>
2012-01-04 17:09:40 +0100

    naa ogsaa signert

--- SimpleAuthishModule/Properties/AssemblyInfo.cs
+++ SimpleAuthishModule/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SimpleAuthishModule")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("None")]
 [assembly: AssemblyProduct("SimpleAuthishModule")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © None 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f3401f30-46fd-4529-b391-fac372fb22c3")]
-
+[assembly: AssemblyKeyFile("SigningKey.snk")]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
